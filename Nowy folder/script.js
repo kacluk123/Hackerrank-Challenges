f5bd0ee8877b5bfdbea8ef108efe5f9d9f4d5ab3
Kacper Łukasik <sakuy124@gmail.com>
2018-11-24 14:48:21 +0100

    .

---
+++ Nowy folder/script.js
@@ -0,0 +1,17 @@
+function funnyString(s) {
+        const arr = s.split('').map((el) => `${el}`.charCodeAt(0))
+        const arrReverse = [...arr].reverse();
+        let emptyArr = [];
+        let emptyArrReverse = [];
+        for (let i = 0; i < arr.length; i++) {
+                let x = arr[i + 1] - arr[i]
+                let y = arrReverse[i - 1] - arrReverse[i]
+                isNaN(y) !== true ? emptyArrReverse = [...emptyArrReverse, Math.abs(y)] : null
+                isNaN(x) !== true ? emptyArr = [...emptyArr, Math.abs(x)] : null
+        }
+
+        return JSON.stringify(emptyArr) === JSON.stringify(emptyArrReverse) ? 'Funny' : "Not Funny"
+}
+
+
+console.log(funnyString("acxz"))
