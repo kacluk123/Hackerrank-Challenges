bfe015996bbc53abab1a7bfa9687005d9a92b1e0
Kacper Łukasik <sakuy124@gmail.com>
2018-12-09 19:55:30 +0100

    Edit

--- d/script.js
+++ d/script.js
@@ -1,8 +1,34 @@
-function kaprekarNumbers(p, q) {
-    const arr = Array.from({length: q}, (v, k) => k+p+1)
-        .map(el => [(el * el).toString().substr(0, el.toString().length),
-            (el * el).toString().substr(el.toString().length, (el * el).toString().length)])
-    return arr
-}
-console.log(kaprekarNumbers(6, 100))
+// function kaprekarNumbers(p, q) {
+//     const arr = Array(q).fill(0).map((e,i)=>i+p).map(el=> (el*el).toString())
+//     return arr
+// }
+// console.log(kaprekarNumbers(1,100))
 
+
+
+    // .map(el => [(el * el).toString().substr(0, el.toString().length),
+    //     (el * el).toString().substr(el.toString().length, (el * el).toString().length)])
+function kaprekarNumbers(p, q) {
+    let arr = [...Array(p)]
+    for(let i = p; i <= q; i++){
+        let number = (i*i).toString()
+        number.length === 1 ? arr = [...arr, [Number(number)]] :
+            arr = [...arr, [Number(number.substr(0, i.toString().length)),
+                Number(number.substr(i.toString().length, number.length)) ]]
+    }
+    let arr2 = []
+    arr.forEach((el,i)=>{
+        if(el !== undefined){
+            let x = el.reduce((a,b)=>{
+                return a + b
+            }, 0)
+            if(x === i){
+                arr2 = [...arr2, i]
+            }
+
+        }
+
+    })
+return arr2
+}
+console.log(kaprekarNumbers(1,100))
